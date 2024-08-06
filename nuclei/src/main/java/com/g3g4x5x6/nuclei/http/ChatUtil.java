746a2b9bc7a8aaf20dc8748ed2c3989326303c67
G3G4X5X6 <87740076+G3G4X5X6@users.noreply.github.com>
2024-08-06 22:23:52 +0800

    add .vscode

--- nuclei/src/main/java/com/g3g4x5x6/nuclei/http/ChatUtil.java
+++ nuclei/src/main/java/com/g3g4x5x6/nuclei/http/ChatUtil.java
@@ -7,14 +7,16 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ChatUtil {
-    private static final FeignClient client = FeignClientConfiguration.createClient(NucleiConfig.getProperty("nuclei.templates.sync.url"));
+    private static final FeignClient client = FeignClientConfiguration
+            .createClient(NucleiConfig.getProperty("nuclei.templates.sync.url"));
 
     public static String chat(String humanMessage) {
         ChatApiRequest chatApiRequest = new ChatApiRequest();
         chatApiRequest.setHumanMessage(humanMessage);
 
-        ChatApiResponse chatApiResponse = client.openaiChat(NucleiConfig.getProperty("nuclei.templates.sync.auth.value"), chatApiRequest);
-        log.info("Ai_message: {}", chatApiResponse.getAiMessage());
+        ChatApiResponse chatApiResponse = client
+                .openaiChat(NucleiConfig.getProperty("nuclei.templates.sync.auth.value"), chatApiRequest);
+        log.info("AiMessage: {}", chatApiResponse.getAiMessage());
 
         return chatApiResponse.getAiMessage();
     }
